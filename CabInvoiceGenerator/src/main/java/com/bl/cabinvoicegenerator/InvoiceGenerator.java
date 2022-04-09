c3a7d581ed5965d664646d13812f99081fd99f44
Chougulenik <nikitachougule47@gmail.com>
2022-04-09 18:20:23 -0400

    added

--- CabInvoiceGenerator/src/main/java/com/bl/cabinvoicegenerator/InvoiceGenerator.java
+++ CabInvoiceGenerator/src/main/java/com/bl/cabinvoicegenerator/InvoiceGenerator.java
@@ -11,14 +11,15 @@ public class InvoiceGenerator {
 		this.rideRepository = new RideRepository();
 	}
 	
-	public double calculateFare(double distance, int time) {
-		return Math.max(MINIMUM_FARE, distance * COST_PER_KM + time * COST_PER_TIME);
+	public double calculateFare(MultipalRides ride) {
+		return Math.max(ride.rideCategory.minFare,
+				ride.getDistance() * ride.rideCategory.costPerKm + ride.getTime() * ride.rideCategory.costPerTime);
 	}
-	
+
 	public EnhancedInvoice calculateFare(MultipalRides[] rides) {
 		double totalFare = 0;
 		for (MultipalRides ride : rides) {
-			totalFare += calculateFare(ride.getDistance(), ride.getTime());
+			totalFare += calculateFare(ride);
 		}
 		return new EnhancedInvoice(rides.length, totalFare);
 	}
--- CabInvoiceGenerator/src/main/java/com/bl/cabinvoicegenerator/MultipalRides.java
+++ CabInvoiceGenerator/src/main/java/com/bl/cabinvoicegenerator/MultipalRides.java
@@ -4,9 +4,11 @@ public class MultipalRides {
 	
 	private double distance;
 	private int time;
+	public RideCategories rideCategory;
 
 	
-	public MultipalRides(double distance, int time) {
+	public MultipalRides(RideCategories rideCategory,double distance, int time) {
+		this.rideCategory = rideCategory;
 		this.distance = distance;
 		this.time = time;
 	}
